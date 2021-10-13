6c4de33c8a2fabd72b2a874cc01ea98323ea6b9a
my <harsharajgopal37@gmail.com>
2021-10-13 08:32:41 -0700

    commit

---
+++ Basics/Checkdatatypes.py
@@ -0,0 +1,24 @@
+a = 5
+b = 'Harsha'
+c = 10.56
+d = [1301343849]
+e = {1,2,3}
+f = (1,2)
+g = {"Harsha":12}
+
+
+# print data type for a variable
+print(type(a))
+# print data type for b variable
+print(type(b))
+# print data type for c variable
+print(type(c))
+# print data type for d variable
+print(type(d))
+# print data type for e variable
+print(type(e))
+# print data type for f variable
+print(type(f))
+# print data type for g variable
+print(type(g))
+
