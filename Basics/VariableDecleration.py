6969cc6ce5c07b590c7bfb9d1e4b09166b28cc97
my <harsharajgopal37@gmail.com>
2021-10-13 07:41:23 -0700

    commit

---
+++ Basics/VariableDecleration.py
@@ -0,0 +1,8 @@
+from calendar import firstweekday
+
+firstname = ("SriHarsha")
+secondname = ("Rompicherla")
+
+# Adding both Variables and print
+
+print("My Full Name is: " + firstname + ' ' + secondname)
