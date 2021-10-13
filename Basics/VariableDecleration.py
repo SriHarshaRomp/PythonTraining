706ec24a95fcb83c831209340724995718811f5f
my <harsharajgopal37@gmail.com>
2021-10-13 09:11:19 -0700

    commit

--- Basics/VariableDecleration.py
+++ Basics/VariableDecleration.py
@@ -3,7 +3,8 @@ from calendar import firstweekday
 firstname = ("SriHarsha")
 secondname = ("Rompicherla")
 a,b,c = 1,'ram',2.5
-# Adding both Variables and print
+# print different data type varaibles in one decleration
 print(a,b,c)
+# Adding both Variables and print
 print("My Full Name is: " + firstname + ' ' + secondname)
 
